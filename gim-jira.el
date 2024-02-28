b5ad297e5629954ca29da94448bb4c1f49fbc1da
George Mauer <gmauer@gmail.com>
2024-02-27 22:37:04 -0600

    create/update seems to be working

--- gim-jira.el
+++ gim-jira.el
@@ -1,8 +1,8 @@
-;;; gim-jira.el --- A better Jira interface...in emacs.
+;;; gim-jira.el --- A better Jira interface
 
 ;; Author: George Mauer <gmauer@gmail.com>
 ;; Version: 0.1.0
-;; Package-Requires: ((emacs "29.1") (dash "20240103.70123") (request "20230127.417")
+;; Package-Requires: ((emacs "24.4") (dash "20240103.70123") (request "20230127.417"))
 ;; Keywords: jira
 ;; URL: https://github.com/togakangaroo/gim-jira
 
@@ -17,17 +17,17 @@
 (require 'request)
 
 (defcustom gim-jira/jira-instance-url (getenv "JIRA_INSTANCE_URL")
-  "URL for jira"
+  "URL for jira."
   :type 'string
   :group 'gim-jira)
 
 (defcustom gim-jira/jira-api-token (getenv "JIRA_API_TOKEN")
-  "JIRA API token used for authentication. https://support.atlassian.com/atlassian-account/docs/manage-api-tokens-for-your-atlassian-account/"
+  "JIRA API token used for authentication. https://support.atlassian.com/atlassian-account/docs/manage-api-tokens-for-your-atlassian-account/."
   :type 'string
   :group 'gim-jira)
 
-(defcustom gim-jira/jira-username (getenv "JIRA_USERNAME")
-  "JIRA username to use for authentication"
+(defcustom gim-jira/jira-username (getenv "JIRA_API_USERNAME")
+  "JIRA username to use for authentication."
   :type 'string
   :group 'gim-jira)
 
@@ -43,41 +43,62 @@
 
 ;;;###autoload
 (defun gim-jira/standard-headers ()
-  "Standard set of headers - including authentication - to be appended to all requests"
+  "Standard set of headers - including authentication - to be appended to all requests."
   `(("Content-Type" . "application/json")
-    ("Authorization" . ,(--> (format "%s:%s" gim-jira/jira-api-username gim-jira/jira-api-token)
+    ("Authorization" . ,(--> (format "%s:%s" gim-jira/jira-username gim-jira/jira-api-token)
                              (base64-encode-string it 't)
                              (format "Basic %s" it)))))
 
-(cl-defun gim-jira/create-or-update-issue-from-heading (heading &optional (buffer (current-buffer)))
-  "For heading with the given text, either create or update the heading from the "
+(cl-defun gim-jira/create-or-update-issue-from-heading (&optional heading-pos (buffer (current-buffer)))
+  "For heading at the char, either create or update the jira issue in the heading denoted by the position. If no heading-pos provided, use the heading we are currently in."
+  (interactive)
+
   (save-excursion
     (cl-block 'gim-jira/create-or-update-issue-from-heading
-        (--> heading
-            (org-find-exact-headline-in-buffer it buffer 't)
-            (goto-char it))
-      (org-next-block 1)
-      (let* ((issue-description (org-element-property :value (org-element-at-point)))
-             (parent-issue-key (org-entry-get (point) gim-jira/property-for-jira-issue-parent-key 't))
-             (current-issue-key (org-entry-get (point) gim-jira/property-for-jira-issue-key 't))
-             (is-update current-issue-key)
-             (verb (if is-update "PUT" "POST"))
-             (extra-fields (unless is-update
-                             `((summary . ,heading)
-                               (parent . ((key . ,parent-issue-key)))
-                               (project . ((key . "BI")))
-                               (issuetype . ((name . "Tech Task"))))))
-             (payload `((fields . ((description . ,issue-description)
-                                   . ,extra-fields)))))
-        (with-temp-buffer
-          (insert (pp-to-string payload))
-          (emacs-lisp-mode)
-          (read-only-mode)
-          (pop-to-buffer (current-buffer))
-          (unless (y-or-n-p "Would you like to continue with the operation? ")
-            (cl-return-from 'gim-jira/create-or-update-issue-from-heading)))
-        (--> payload
-             json-encode)))))
+      (unless heading-pos
+        (setq heading-pos (org-back-to-heading)))
+      (-let* ((element-title (org-element-property :title (org-element-at-point)))
+              ((heading ...) (-flatten-n 3 (list element-title)))) ; element title might be a string or a list where we need the first element
+        (goto-char heading-pos)
+        (org-next-block 1)
+        (-let* ((issue-description (org-element-property :value (org-element-at-point)))
+                (parent-issue-key (org-entry-get (point) gim-jira/property-for-jira-issue-parent-key 't))
+                (current-issue-key (org-entry-get (point) gim-jira/property-for-jira-issue-key 't))
+                ((confirm-question verb extra-fields) (cond (current-issue-key `(,(format "Would you like to update issue %s? " current-issue-key)
+                                                                                 "PUT"
+                                                                                 nil))
+                                                            (:else `("Would you like to create this issue?"
+                                                                     "POST"
+                                                                     ,`((summary . ,heading)
+                                                                        (parent . ((key . ,parent-issue-key)))
+                                                                        (project . ((key . "BI")))
+                                                                        (issuetype . ((name . "Tech Task"))))))))
+                (payload `((fields . ((description . ,issue-description)
+                                      . ,extra-fields))))
+                (foo (message (format "PAYLOAD %s" payload)))
+                ;; (bar (edebug))
+                (json-payload (json-encode payload)))
+          (with-temp-buffer
+            (insert json-payload)
+            (json-mode)
+            (json-pretty-print-buffer)
+            (read-only-mode)
+            (pop-to-buffer (current-buffer))
+            (unless (y-or-n-p confirm-question)
+              (cl-return-from 'gim-jira/create-or-update-issue-from-heading)))
+          (-let* ((url (format "%s/rest/api/2/issue/%s" gim-jira/jira-instance-url
+                               (or current-issue-key "")))
+                  (res (request url 
+                           :type verb
+                           :data json-payload
+                           :sync t
+                           :parser 'json-read
+                           :headers (gim-jira/standard-headers)))
+                  (res-data (request-response-data res))
+                  (new-issue-key (alist-get 'key res-data)))
+            (message (format "Response: %s" res-data))
+            (when new-issue-key
+              (org-entry-put nil gim-jira/property-for-jira-issue-key new-issue-key))))))))
 
 (provide 'gim-jira)
 
