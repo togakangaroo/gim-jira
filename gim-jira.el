118cb1f9380193b2986bad4787549f702db3df5d
George Mauer <gmauer@gmail.com>
2024-02-29 18:35:48 -0600

    refactor for clarity

--- gim-jira.el
+++ gim-jira.el
@@ -64,53 +64,52 @@
   (interactive)
 
   (save-excursion
-    (cl-block 'gim-jira/create-or-update-issue-from-heading
-      (unless heading-pos
-        (setq heading-pos (org-back-to-heading)))
-      (-let* ((element-title (org-element-property :title (org-element-at-point)))
-              ((heading ...) (-flatten-n 3 (list element-title)))) ; element title might be a string or a list where we need the first element
-        (goto-char heading-pos)
-        (org-next-block 1)
-        (-let* ((description-prefix (gim-jira/-org-get-current-property gim-jira/property-for-issue-description-prefix :default ""))
-                (parent-issue-key (gim-jira/-org-get-current-property gim-jira/property-for-jira-issue-parent-key))
-                (current-issue-key (gim-jira/-org-get-current-property gim-jira/property-for-jira-issue-key))
-                (issue-description (format "%s%s" description-prefix
-                                                  (org-element-property :value (org-element-at-point))))
-                ((confirm-question verb extra-fields) (cond (current-issue-key `(,(format "Would you like to update issue %s? " current-issue-key)
-                                                                                 "PUT"
-                                                                                 nil))
-                                                            (:else `("Would you like to create this issue?"
-                                                                     "POST"
-                                                                     ,`((summary . ,heading)
-                                                                        (parent . ((key . ,parent-issue-key)))
-                                                                        (project . ((key . "BI")))
-                                                                        (issuetype . ((name . "Tech Task"))))))))
-                (payload `((fields . ((description . ,issue-description)
-                                      . ,extra-fields))))
-                (foo (message (format "PAYLOAD %s" payload)))
-                ;; (bar (edebug))
-                (json-payload (json-encode payload)))
-          (with-temp-buffer
-            (insert json-payload)
-            (json-mode)
-            (json-pretty-print-buffer)
-            (read-only-mode)
-            (pop-to-buffer (current-buffer))
-            (unless (y-or-n-p confirm-question)
-              (cl-return-from 'gim-jira/create-or-update-issue-from-heading)))
-          (-let* ((url (format "%s/rest/api/2/issue/%s" gim-jira/jira-instance-url
-                               (or current-issue-key "")))
-                  (res (request url 
-                         :type verb
-                         :data json-payload
-                         :sync t
-                         :parser 'json-read
-                         :headers (gim-jira/standard-headers)))
-                  (res-data (request-response-data res))
-                  (new-issue-key (alist-get 'key res-data)))
-            (message (format "Response: %s" res-data))
-            (when new-issue-key
-              (org-entry-put nil gim-jira/property-for-jira-issue-key new-issue-key))))))))
+    (unless heading-pos
+      (setq heading-pos (org-back-to-heading)))
+    (-let* ((element-title (org-element-property :title (org-element-at-point)))
+            ((heading ...) (-flatten-n 3 (list element-title)))) ; element title might be a string or a list where we need the first element
+      (goto-char heading-pos)
+      (org-next-block 1)
+      (-let* ((description-prefix (gim-jira/-org-get-current-property gim-jira/property-for-issue-description-prefix :default ""))
+              (parent-issue-key (gim-jira/-org-get-current-property gim-jira/property-for-jira-issue-parent-key))
+              (current-issue-key (gim-jira/-org-get-current-property gim-jira/property-for-jira-issue-key))
+              (issue-description (format "%s%s" description-prefix
+                                       (org-element-property :value (org-element-at-point))))
+              ((confirm-question verb extra-fields) (cond (current-issue-key `(,(format "Would you like to update issue %s? " current-issue-key)
+                                                                               "PUT"
+                                                                               nil))
+                                                          (:else `("Would you like to create this issue?"
+                                                                   "POST"
+                                                                   ,`((summary . ,heading)
+                                                                      (parent . ((key . ,parent-issue-key)))
+                                                                      (project . ((key . "BI")))
+                                                                      (issuetype . ((name . "Tech Task"))))))))
+              (payload `((fields . ((description . ,issue-description)
+                                    . ,extra-fields))))
+              (foo (message (format "PAYLOAD %s" payload)))
+              ;; (bar (edebug))
+              (json-payload (json-encode payload)))
+        (with-temp-buffer
+          (insert json-payload)
+          (json-mode)
+          (json-pretty-print-buffer)
+          (read-only-mode)
+          (pop-to-buffer (current-buffer))
+          (unless (y-or-n-p confirm-question)
+            (cl-return-from gim-jira/create-or-update-issue-from-heading)))
+        (-let* ((url (format "%s/rest/api/2/issue/%s" gim-jira/jira-instance-url
+                             (or current-issue-key "")))
+                (res (request url 
+                       :type verb
+                       :data json-payload
+                       :sync t
+                       :parser 'json-read
+                       :headers (gim-jira/standard-headers)))
+                (res-data (request-response-data res))
+                (new-issue-key (alist-get 'key res-data)))
+          (message (format "Response: %s" res-data))
+          (when new-issue-key
+            (org-entry-put nil gim-jira/property-for-jira-issue-key new-issue-key)))))))
 
 (provide 'gim-jira)
 
